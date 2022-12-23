ddcdb7621df33f7e2550f262c843688fb307a7d8
MohamadAboud <mohamadaboud663@gmail.com>
2022-12-23 13:30:39 +0300

    fix some errors

--- utils/file_manipulation.py
+++ utils/file_manipulation.py
@@ -1,5 +1,6 @@
 from os import makedirs
 import os
+from utils.dev import Developer
 
 def isExists(path):
     return os.path.exists(path) == True
@@ -12,11 +13,18 @@ def CrateFolder(path):
 def ChangeNameFolder(path,oldname, newname):
     oldpath = os.path.join(path,oldname)
     newpath = os.path.join(path,newname)
-    if isExists(oldpath) == True:
+
+    if isExists(newpath): raise FileExistsError()
+
+    if isExists(oldpath):
+        Developer.log(f"The folder will be rename\n-> from: \'{oldpath}\'\n-> to\'{newpath}\'", mode='warning')
         os.rename(oldpath, newpath)
 
 def DeleteFolder(path):
+
+
     if isExists(path) == True:
+        Developer.log(f"The folder will be deleted\n-> path: \'{path}\'", mode='warning')
         for file in os.listdir(path):  # loop to delete each file in folder
             os.remove(f'{path}/{file}')  # delete file
 
