30583b7113683968a356c0c92441dfed95a20548
MohamadAboud <mohamadaboud663@gmail.com>
2022-12-25 19:30:04 +0300

    add colors to log method

--- utils/dev.py
+++ utils/dev.py
@@ -1,4 +1,13 @@
 
+
+class Style:
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class Developer:
     isTesting = True
 
@@ -13,4 +22,13 @@ class Developer:
         :return: If it is in developer mode it will print
         """
         if Developer.isTesting:
-            print(*args, sep=' ', end='\n', file=None)
+            if mode == "info":
+                print(f"{Style.BOLD}",*args,f"{Style.ENDC}", sep=' ', end='\n', file=None)
+            elif mode == "error":
+                print(f"{Style.ERROR}",*args,f"{Style.ENDC}", sep=' ', end='\n', file=None)
+            elif mode == "warning":
+                print(f"{Style.WARNING}",*args,f"{Style.ENDC}", sep=' ', end='\n', file=None)
+            elif mode == "test":
+                print(f"{Style.UNDERLINE}",*args,f"{Style.ENDC}", sep=' ', end='\n', file=None)
+            else:
+                print(*args, sep=' ', end='\n', file=None)
